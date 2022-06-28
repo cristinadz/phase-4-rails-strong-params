a25858e76135704a1882a47c4f5b51fd1694905c
Cristina Diaz <diazcristina.la@gmail.com>
2022-06-28 15:34:41 -0700

    commit tree faf297e5b5a240a72de5f775a393b7657f65a85d

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,17 +1,13 @@
 class BirdsController < ApplicationController
-
+  wrap_parameters format: []
+  
   # GET /birds
   def index
     birds = Bird.all
     render json: birds
   end
 
-  # POST /birds
-  def create
-    bird = Bird.create(name: params[:name], species: params[:species])
-    render json: bird, status: :created
-  end
-
+ 
   # GET /birds/:id
   def show
     bird = Bird.find_by(id: params[:id])
@@ -22,4 +18,17 @@ class BirdsController < ApplicationController
     end
   end
 
+   # POST /birds
+   def create
+    bird = Bird.create(bird_params)
+    render json: bird, status: :created
+  end
+
+    private 
+
+    # all methods below here are private
+
+    def bird_params
+      params.permit(:name, :species)
+    end
 end
